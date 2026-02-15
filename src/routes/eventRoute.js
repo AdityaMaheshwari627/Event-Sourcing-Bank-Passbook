99c58a5512dd508308458e008b7574f4c235288e
AdityaMaheshwari627 <aditya27mh@gmail.com>
2026-02-15 20:18:00 +0530

    Update correct

--- src/routes/eventRoute.js
+++ src/routes/eventRoute.js
@@ -5,9 +5,9 @@ const controller = require("../controllers/eventController");
 router.post("/deposit", controller.deposit);
 router.post("/withdraw", controller.withdraw);
 
-module.exports = router;
-
 router.get("/balance", controller.getBalance);
 router.get("/passbook", controller.getPassbook);
 
 router.post("/snapshot", controller.createSnapshot);
+
+module.exports = router;
